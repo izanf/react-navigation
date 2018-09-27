047cf6f1b1835650eec4280e9b3f7d37d2aa3e31
Brent Vatne <brentvatne@gmail.com>
2018-09-27 13:14:13 -0700

    Publish a first alpha, far from ready still

--- examples/NavigationPlayground/App.js
+++ examples/NavigationPlayground/App.js
@@ -2,9 +2,9 @@ import { Platform } from 'react-native';
 import { useScreens } from 'react-native-screens';
 
 // Uncomment this to use react-native-screens
-if (Platform.OS === 'android') {
-  useScreens();
-}
+// if (Platform.OS === 'android') {
+//  useScreens();
+// }
 
 import App from './js/App';
 export default App;
